a4f6e95fc13acb7698d51c5f1ee66301b9d9225c
Zach Gover <zachary.w.gover@gmail.com>
2021-03-29 11:03:53 -0500

    - Continued designing site-core interface. - Committing to upgrading NX.

--- libs/site-core/src/lib/site-core.ts
+++ libs/site-core/src/lib/site-core.ts
@@ -2,29 +2,41 @@ import EventEmitter from 'events'
 import type DdfSchema from '@data-driven-forms/react-form-renderer/common-types/schema'
 
 
-export type SiteElementProps = Record<string, unknown>
-export type SiteComponentResolveProps = <T>(...args: T[]) => Partial<SiteElementProps> | void
-export type SiteComponentPropsSchema = DdfSchema
-export type SiteComponentNestingRestriction = [
-  RestrictionType,
-  SiteComponent['id'][]
-]
+const PKG_VERSION = JSON.stringify(process.env.PKG_VERSION ?? 'N/A')
+const PRODUCTION = process.env.NODE_ENV === 'production'
 
-export enum RestrictionType {
+
+export enum RestrictType {
+  NONE,
   LIMIT    = 'limit',
   DISALLOW = 'disallow',
 }
+export enum ScEvent {
+  INSTANCE_CREATED = 'site_core.created-instance'
+}
+export enum ScElementType {
+  TAG_NAME = 'tagName',
+  SITE_COMPONENT = 'comp',
+}
 
-export interface SiteComponent {
-  id: string
+export type ScComponentResolveProps = <T>(...args: T[]) => Partial<ScElementProps> | void
+export type ScComponentPropsSchema = DdfSchema
+export type ScComponentNestingRestriction = [RestrictType, string[]]
+
+export type ScElementProps = Record<string, unknown>
+
+
+export interface ScComponent {
+  _id: string
+  ClassFn: any
   name: string
   title?: string
   subtitle?: string
   description?: string
   icon?: string
-  resolveProps?: SiteComponentResolveProps
-  defaultProps?: Partial<SiteElementProps>
-  propsSchema?: SiteComponentPropsSchema
+  resolveProps?: ScComponentResolveProps
+  defaultProps?: Partial<ScElementProps>
+  propsSchema?: ScComponentPropsSchema
   options?: {
     disableActions?: boolean
     disableBadge?: boolean
@@ -36,28 +48,62 @@ export interface SiteComponent {
     disableOutline?: boolean
     disableRemoving?: boolean
     disableSelecting?: boolean
-    restrictChildren?: SiteComponentNestingRestriction
-    restrictParents?: SiteComponentNestingRestriction
+    restrictChildren?: ScComponentNestingRestriction
+    restrictParents?: ScComponentNestingRestriction
   }
 }
 
-export interface SiteElement {
-  id: string
-  component: {
-    type?: 'dom' | 'sc'
-    cid?: SiteComponent['id']
-    tagName?: string
-  }
-  children?: SiteElement[]
-  props: SiteElementProps
-  name?: string
+export interface ScElement {
+  _id: string
+  comp?: ScComponent | string
+  children?: ScElement[]
+  props: ScElementProps
   temp?: boolean
-  pid?: SiteElement['id'] /* Parent SiteElement ID */
+  parent?: string
+  name?: string
 }
 
 export class SiteCore {
-  public static version: string
-  private static event: EventEmitter = new EventEmitter()
+
+  public static readonly version: string = PKG_VERSION
+  public static readonly production: boolean = PRODUCTION
+  public static readonly event: EventEmitter = new EventEmitter()
+
+  private static instance: SiteCore
+  private static components: Map<string, [
+    options: Omit<ScComponent, 'ClassFn'>,
+    ClassFn: Pick<ScComponent, 'ClassFn'>
+  ]> = new Map()
+
+
+  /**
+   * Get the currently living singleton instance
+   * @throws
+   * @returns {SiteCore} instance
+   */
+  public static getInstance(): SiteCore {
+    if (this.instance instanceof SiteCore) {
+      return this.instance
+    }
+    throw new Error('Instance doesn\'t exist! You must call createInstance(...) first!')
+  }
+
+  /**
+   * Creates a new singleton instance of SiteCore
+   * @throws
+   */
+  public static createInstance() {
+    if (this.instance instanceof SiteCore) {
+      throw new Error('Instance exist! You have already created an instance.')
+    }
+    this.instance = new SiteCore()
+    this.event.emit(ScEvent.INSTANCE_CREATED)
+  }
+
+
+
+
+
 }
 
 export function siteCore() {
