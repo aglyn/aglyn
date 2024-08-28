82a0d64cbbd06a984adbb18992cdde68f6b61e22
Zach Gover <9044773+zgover@users.noreply.github.com>
2024-08-28 10:28:05 -0500

    fix: tsconfig forced emotion jsx source on all apps
    - app directory in nextjs apps now work as expected

---
+++ apps/console/app/unstable/about/page.tsx
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2024 Aglyn LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import NextLink from 'next/link'
+import * as React from 'react'
+// import Copyright from '@/components/Copyright'
+// import ProTip from '@/components/ProTip'
+
+export default function About() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI - Next.js example in TypeScript
+        </Typography>
+        <Box sx={{ maxWidth: 'sm' }}>
+          <Button variant="contained" component={NextLink} href="/unstable">
+            Go to the home page
+          </Button>
+        </Box>
+        {/*<ProTip />*/}
+        {/*<Copyright />*/}
+      </Box>
+    </Container>
+  )
+}
